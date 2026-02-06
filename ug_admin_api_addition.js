86469fafa3579fd9db87fa399d2fff2afbd8352d
phillikali-glitch <phillikali@gmail.com>
2026-02-06 12:36:56 +0300

    Add files via upload

---
+++ ug_admin_api_addition.js
@@ -0,0 +1,7 @@
+const config = {
+    apiEndpoints: {
+        primary: "https://api.uganda-data.org/v1",
+        backup: "https://geodata.ubos.org/api",
+        openStreetMap: "https://overpass-api.de/api/interpreter"
+    }
+};
